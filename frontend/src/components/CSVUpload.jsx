b71d11514668f778a52cb8e77b7ada1ed1e09e75
Abhishek Kumar Singh <krabhisheksingh107@gmail.com>
2026-06-14 19:05:32 +0530

    Update API URL for production

--- frontend/src/components/CSVUpload.jsx
+++ frontend/src/components/CSVUpload.jsx
@@ -62,7 +62,7 @@ const CSVUpload = ({ setResult }) => {
 
           // API call karo
           const response = await fetch(
-            "http://127.0.0.1:5000/predict",
+            "https://breast-cancer-api-n8t2.onrender.com/predict",
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
